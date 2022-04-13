e8ab0a3dae196f5fa6f36f0829c61719151a2f85
Spencer <dresmanns@gmail.com>
2022-04-13 08:42:53 -0400

    work

---
+++ app.js
@@ -0,0 +1,27 @@
+var a = 10
+var b = 30
+
+function addSum(params1, param2) {
+    console.log(params1 + param2);
+    return params1 + param2
+}
+addSum(a,b)
+
+function multiplySum(params1, param2) {
+    console.log(params1, param2);
+    return params1 * param2
+    
+}
+multiplySum(a,b)
+
+function subSum(params1, param2){
+    console.log(params1, param2);
+    return params1 - param2
+}
+subSum(a,b)
+
+function divideSum(params1, param2){
+    console.log(params1, param2);
+    return params1 / param2
+}
+divideSum(a,b)
